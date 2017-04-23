5656f2fff335428bd9e1459a506b02c22b3d4916
User <sanik3d@gmail.com>
2017-04-24 02:02:46 +0400

    beginning

---
+++ src/main/java/model/GeographyMap.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ * Created by User on 23.04.2017
+ */
+public class GeographyMap {
+    private double[][] gridHeights;
+    private int minX;
+    private int minY;
+    private int maxX;
+    private int maxY;
+    private int gridStep;
+
+    public GeographyMap(Coordinate[] borderCoordinates, int gridStep, int hY) {
+        findMinAndMaxCoordinates(borderCoordinates);
+        this.gridStep = gridStep;
+        makeGrid();
+
+    }
+
+    private void findMinAndMaxCoordinates(Coordinate[] borderCoordinates) {
+        minX = Integer.MAX_VALUE;
+        minY = Integer.MAX_VALUE;
+        maxX = Integer.MIN_VALUE;
+        maxY = Integer.MIN_VALUE;
+        for (Coordinate coordinate : borderCoordinates) {
+            minX = Math.min(minX, coordinate.x);
+            minY = Math.min(minY, coordinate.y);
+            maxX = Math.max(maxX, coordinate.x);
+            maxY = Math.max(maxY, coordinate.y);
+        }
+    }
+
+    private void makeGrid() {
+        int size = (maxX - minX) / gridStep;
+        gridHeights = new double[size][size];
+    }
+
+    public double[][] interpolate(Function2Args[] functions){
+        for(int i = 0; i < )
+    }
+
+}
